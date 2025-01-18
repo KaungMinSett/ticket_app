771624a7b618933fe51f61f3be1b1509b7b25913
KaungMinSett <KaungMinSat@my.uopeople.edu>
2025-01-18 13:52:49 +0630

    all ticket view finished

---
+++ lib/screens/all_tickets.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/utils/all_json.dart';
+import 'package:ticket_app/base/widgets/ticket_view.dart';
+
+class AllTickets extends StatelessWidget {
+  const AllTickets({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.black,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+
+
+        ),
+        forceMaterialTransparency: true,
+        title: const Text("All Tickets"),
+      ),
+      body: ListView(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+                children: ticketList
+                    .map((singleTicket) =>
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                    child: TicketView(ticket: singleTicket, wholeScreen: true,)))
+                    .toList()),
+          )
+        ],
+      ),
+    );
+  }
+}
